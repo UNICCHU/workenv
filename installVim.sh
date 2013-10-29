467a5e0393ed4a8e1c21020555c7852176c41849
ackeack <uniccus@gmail.com>
2013-10-29 11:03:32 +0800

    Reconstruct the folder structure 	new file:   env/.screenrc 	deleted:    installVim.sh 	new file:   install_vim.sh 	new file:   vim/.vimrc

--- installVim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -rf .vim ~/
-cp -rf .vimrc  ~/
-
-echo 'SYS: install enhanced successfully'
---
+++ install_vim.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# ackack's workenv 
+
+function func_execute_and_check() {
+    echo 'INFO: Process' $1'...'
+    $1
+    if [ $? -ne 0 ]; then
+        echo 'ERROR: Operation failed:' $1
+    fi
+}
+
+func_execute_and_check 'source cmds/submod_sync.sh'
+func_execute_and_check 'cp -rf .vim $HOME'
+func_execute_and_check 'cp -rf .vimrc $HOME'
+echo 'SYS: install vim successfully'
