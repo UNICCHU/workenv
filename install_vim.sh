f5a22b446012aa17f515919ce364a363d9b3c3e1
ackeack <ackeack.gmail.com>
2013-10-30 17:19:56 +0800

    fix and fix

--- install_vim.sh
+++ install_vim.sh
@@ -12,8 +12,8 @@ function func_execute_and_check() {
 
 func_execute_and_check 'source cmds/submod_sync.sh'
 func_execute_and_check 'source cmds/submod_upgrade.sh'
-mv -rf $HOME/.vim $HOME/.vim_bak
-mv -rf $HOME/.vimrc $HOME/.vimrc_bak
+mv -f $HOME/.vim $HOME/.vim_bak
+mv -f $HOME/.vimrc $HOME/.vimrc_bak
 cp -rf vim/.vim $HOME
 cp -rf vim/.vimrc $HOME
 echo 'SYS: install vim successfully'
